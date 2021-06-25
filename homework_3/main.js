985ae0dfc3c6ffc56ddc8f9de7c3123064a7475e
nat-gette <natalagette@gmail.com>
2021-06-25 16:10:41 +0500

    homework 3(4,5)

--- homework_3/main.js
+++ homework_3/main.js
@@ -25,3 +25,47 @@ function fact(a) {
 
 console.log(fact (10));
 
+// Функция, которая возвращает строку перевертыш reverseString('test')="tset"
+
+
+function reverseString( string, handler) {
+    let newString = '';
+    
+    for ( let i = string.length -1; i >= 0; i--){
+        newString += handler(string[i]); 
+    }
+    console.log(newString);
+    return newString;
+}
+
+reverseString ('test', function (symbol){
+    return  symbol;
+});
+
+// Функция, которая принимает строку из букв, а возвращает строку из юникода букв через пробел
+
+ 
+function rewreireString( string, handler) {
+    let newString = '';
+    
+    for ( let i = 0; i < string.length; i++){
+        newString += handler(string[i]); 
+    }
+    console.log(newString);
+    return newString;
+}
+
+rewreireString ('hello', function (symbol){
+    return symbol.charCodeAt() + ' ';
+});
+
+// Функция-рекурсия, которая выдает каждый символ строки в консоль
+
+function rec() {
+    if (a >  0) {
+       return  rec(a);
+    }
+   return 1;
+}
+
+console.log(rec (a));
