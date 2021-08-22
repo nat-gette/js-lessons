0b486037bc971cd3c757d7a3467edd57388e7e2e
nat-gette <natalagette@gmail.com>
2021-08-22 20:11:45 +0500

    homework5.1, ex 3,4

--- homework_5.2/main.js
+++ homework_5.2/main.js
@@ -1,28 +1,37 @@
 // --------------- DOM задачи---------------
 // 1.Создать функцию, кот принимает 2 элемента. Функция проверяет, является ли первый элемент родителем для второго: isPArent(parent,child)
 
-function isParent (parent, child) {
-    // debugger
-    return !!child.closest(parent.tagName)
-}
-console.log(isParent(document.body.children[0], document.querySelector('mark')));
-console.log(isParent(document.querySelector('ul'), document.querySelector('mark')));
+// function isParent (parent, child) {
+//     // debugger
+//     return !!child.closest(parent.tagName)
+// }
+// console.log(isParent(document.body.children[0], document.querySelector('mark')));
+// console.log(isParent(document.querySelector('ul'), document.querySelector('mark')));
 
 // 2. Получить список всех ссылок внутри ul;
 
-let links = document.querySelectorAll('a');
+// let links = document.querySelectorAll('a');
 
-let linksSort = [];
-for (let i = 0; i < links.length; i++) {
-    if(!links[i].closest('ul')) {
-        linksSort.push(links[i])
-    }
-}
- console.log(linksSort);
+// let linksSort = [];
+// for (let i = 0; i < links.length; i++) {
+//     if(!links[i].closest('ul')) {
+//         linksSort.push(links[i])
+//     }
+// }
+//  console.log(linksSort);
 
 // 3. Найти эл-т, кот находится перед и после списка ul
 
+let list = document.querySelector('ul');
+
+console.log(list.previousSibling, list.nextSibling);
+
+
 // 4. Посчитать кол-во эл-в li в списке
+
+let ollLi = document.querySelectorAll('li')
+
+console.log(ollLi.length)
 
 
 
