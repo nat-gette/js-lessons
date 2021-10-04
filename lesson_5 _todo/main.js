fd5c6e5ac52a0b4796bf4306debd8b798f742fe7
nat-gette <natalagette@gmail.com>
2021-10-04 23:04:36 +0500

    lesson 5, vid 11

--- lesson_5 _todo/main.js
+++ lesson_5 _todo/main.js
@@ -1,8 +1,4 @@
-let tasks = [
-    "выучить js",
-    "выучить Angular 4",
-    "сходить на конфу"
-];
+let tasks = JSON.parse( localStorage.getItem('tasks') )|| [];
 
 
 // let ul = document.querySelector('.list-group');
@@ -51,16 +47,25 @@ let tasks = [
 let ul = document.querySelector('.list-group');
 let form = document.forms['addTodoItem'];
 let inputText = form.elements['todoText'];
+let tasksSuccessAlert = document.querySelector('.alert-tasks');
 
 
 function listTemplate(task) {
     let li = document.createElement('li');
-    li.textContent = task;
     li.className = 'list-group-item d-flex align-items-center';
 
-    let iDelete = document.createElement('i');
-    iDelete.className = 'fas fa-trash-alt delete-item ml-auto';
+    let span = document.createElement('span');
+    span.textContent = task;
 
+
+    let iDelete = document.createElement('i');
+    iDelete.className = 'fas fa-trash-alt delete-item ml-4';
+
+    let iEdit = document.createElement('i');
+    iEdit.className = 'fas fa-edit edit-item ml-auto';
+
+    li.appendChild(span);
+    li.appendChild(iEdit);
     li.appendChild(iDelete);
 
     return li;
@@ -84,9 +89,11 @@ function generateList(tasksArray) {
 function addLists(list) {
     tasks.push(list);
     ul.insertAdjacentElement('afterbegin', listTemplate(list));
+    // add to localStorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function delrtrListItem(target) {
+function deliteListItem(target) {
 
     let parent = target.closest('li');
     let text = parent.textContent;
@@ -98,10 +105,30 @@ function delrtrListItem(target) {
 
 ul.addEventListener('click', function (e) {
     if (e.target.classList.contains('delete-item')) {
-        delrtrListItem( e.target);
+        deliteListItem( e.target);
 
+        showAlert('alert-danger', 'Task has been removed success');
+    } else if (e.target.classList.contains('edit-item')) {
+        let span = e.target.closest('li').querySelector('span');
+        span.setAttribute('contenteditable', true);
+        span.focus();
     }
 });
+
+function showAlert(className, textAlert) {
+
+    tasksSuccessAlert.classList.add('alert-tasks-active');
+    tasksSuccessAlert.classList.add(className);
+    tasksSuccessAlert.textContent = textAlert;
+    
+    setTimeout(() => {
+        tasksSuccessAlert.classList.remove('alert-tasks-active');
+        tasksSuccessAlert.classList.remove(className);
+        tasksSuccessAlert.textContent = '';
+
+    }, 3000);
+
+}
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -112,6 +139,19 @@ form.addEventListener('submit', function (e) {
         inputText.classList.remove('is-invalid')
         addLists(inputText.value);
         form.reset();
+
+        showAlert('alert-success', 'Task added success!')
+        // tasksSuccessAlert.classList.add('alert-tasks-active');
+        // tasksSuccessAlert.classList.add('alert-success');
+        // tasksSuccessAlert.textContent = 'Task added success!';
+        
+        // setTimeout(() => {
+        //     tasksSuccessAlert.classList.remove('alert-tasks-active');
+        //     tasksSuccessAlert.classList.remove('alert-success');
+        //     tasksSuccessAlert.textContent = '';
+
+        // }, 3000);
+        
     }
 
 });
@@ -155,8 +195,64 @@ generateList(tasks);
 //     });
 // }
 
+//--------------------- Cобытия
+// let body = document.body;
+// let taskWrap = document.querySelector('.tasks-wrap');
+// let container  = document.querySelector('.container');
+// let listCard = document.querySelector('.list-card');
+// let cardBody = document.querySelector('.list-card .card-body');
+// let listGroup = document.querySelector('.list-group');
 
-let body = document.body;
-let taskWrap = document.querySelector('.tasks-wrap');
-let container  = document.querySelector('.container');
-let listCard = document.querySelector('.list-card');
+// // погружение события
+// listGroup.addEventListener('click', function (e) {
+//     console.log('.list-group');
+// }, true);
+
+// cardBody.addEventListener('click', function (e) {
+//     console.log('.list-card .card-body');
+// }, true);
+
+// listCard.addEventListener('click', function (e) {
+//     // e.stopImmediatePropagation();
+//     console.log('.list-card');
+// }, true);
+
+// container.addEventListener('click', function (e) {
+//     console.log('.container');
+// }, true);
+
+// taskWrap.addEventListener('click', function (e) {
+//     console.log('.tasks-wrap');
+// }, true);
+
+// body.addEventListener('click', function (e) {
+//     console.log('body');
+// }, true);
+
+
+// // всплытие события
+
+// listGroup.addEventListener('click', function (e) {
+//     console.log('.list-group');
+// });
+
+// cardBody.addEventListener('click', function (e) {
+//     console.log('.list-card .card-body');
+// });
+
+// listCard.addEventListener('click', function (e) {
+//     e.stopImmediatePropagation();
+//     console.log('.list-card');
+// });
+
+// container.addEventListener('click', function (e) {
+//     console.log('.container');
+// });
+
+// taskWrap.addEventListener('click', function (e) {
+//     console.log('.tasks-wrap');
+// });
+
+// body.addEventListener('click', function (e) {
+//     console.log('body');
+// });
