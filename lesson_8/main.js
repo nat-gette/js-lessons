8ccb27169a24d65cb5fc8f80e2b915013219616b
nat-gette <natalagette@gmail.com>
2021-11-25 00:50:49 +0500

    lesson 8, part2

--- lesson_8/main.js
+++ lesson_8/main.js
@@ -26,21 +26,63 @@ const timer = (function() {
     let countdown;
     let timerDisplay;
     let endTime;
+    let alarmSound;
 
     function init(settings) {
+        
+        timerDisplay = document.querySelector(settings.timeLeftSelector);
+        endTime = document.querySelector(settings.timeEndSelector);
 
+        if (settings.alarmSound) {
+            alarmSound = new Audio(settings.alarmSound);
+        }
     };
 
     function start(seconds) {
+        
+        if (!timerDisplay || !endTime) return console.log('Please init module first.');
+        if (!seconds || typeof seconds !== 'number') return console.log('Please provide seconds.');
 
+        // reset timer
+        clearInterval(countdown);
+        // reset sound
+        alarmSound.pause();
+        alarmSound.currentTime = 0;
+
+        const now = Date.now();
+        const then = now + seconds * 1000;
+
+        displayTimeLeft(seconds);
+        displayEndTime(then);
+
+        countdown = setInterval(() => {
+            const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+            if (secondsLeft < 0) {
+                clearInterval(countdown);
+                playSound();
+                return;
+            }
+
+            displayTimeLeft(secondsLeft);
+        }, 1000);
     };
 
     function displayTimeLeft(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const reminderSeconds = seconds % 60;
 
+        const display = `${minutes}:${reminderSeconds < 10 ? '0' : ''}${reminderSeconds}`;
+        document.title = display;
+        timerDisplay.textContent = display;
     };
 
     function displayEndTime(timestamp) {
+        const end = new Date(timestamp);
+        const hour = end.getHours();
+        const minutes = end.getMinutes();
 
+        endTime.textContent = `Be back at ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
     };
 
     function stop() {
@@ -48,10 +90,10 @@ const timer = (function() {
     };
 
     function playSound() {
-
+        alarmSound.play();
     };
 
-    return{
+    return {
         init,
         start,
         stop,
@@ -63,9 +105,19 @@ const timer = (function() {
 
 timer.init({
     timeLeftSelector: ".display__time-left",
-    timeEndSelector: 'display__end-time',
-    alarmSound: 'audio/bell.mp3'
+    timeEndSelector: '.display__end-time',
+    alarmSound: 'audio/message_sound.mp3' ,
 });
+
+// start timer by click
+function startTimer(e) {
+    const seconds = parseInt(this.dataset.time);
+    timer.start(seconds);
+
+    
+};
+
+buttons.forEach(btn => btn.addEventListener('click', startTimer));
 
 
 
